aad6bdc80d2ef72f320f3ee79d97b7a63e548e89
Muhammad Yousuf Amray <140941940+Amrayousuf@users.noreply.github.com>
2025-02-03 22:15:08 -0500

    Completed Project

---
+++ Data and Information Management/Assignment2Part2Queries.sql
@@ -0,0 +1,56 @@
+--1. For each Toronto branch count the number of properties managed by their staff.  
+-- Sequence the output by highest to lowest count
+
+
+
+
+--2. List the details for the branches where the number of employees is more 
+-- than 2 and the average employee salary is less than $70,000
+
+
+
+
+--3. Count the number of different property types located in Waterloo that 
+-- are currently leased.  
+
+
+   
+
+--4. List the details of all leases for properties managed by staff in Toronto branches 
+-- that will expire anytime in 2023. Sequence by expiry date
+
+
+
+
+--5. List the details for owners with more than 2 townhouse (TH) Toronto properties 
+
+
+
+
+--6. List the details for staff who manage townhouse (TH) properties 
+-- that have a current lease. Show each staff only once. Sequence alphabetical by name.
+
+
+
+
+--7. List the details for staff who manage more than 2 properties
+
+
+
+
+--8. List the details for properties that have current leases
+-- managed by staff working in Waterloo branches.
+
+
+
+
+--9 List the details for staff who supervise more than 2 subordinates
+
+
+
+
+--10. List the details for owners that have both detached (DT) properties and townhouses (TH).
+-- Show each owner only once. 
+
+
+
