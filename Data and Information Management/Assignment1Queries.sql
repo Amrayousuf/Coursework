180066aa50a1216da6f9df7897cf18e0b5422068
Muhammad Yousuf Amray <140941940+Amrayousuf@users.noreply.github.com>
2025-02-03 22:18:33 -0500

    Completed Assignment 1

---
+++ Data and Information Management/Assignment1Queries.sql
@@ -0,0 +1,64 @@
+--1. List the details for those movies whose title contains either 'matrix', 'star', or 'moon'
+-- and is made between 2000 and 2020.
+Select * from AssignmentW2021 where title =
+
+
+
+-- 2. List the actor number for each actor and the total amount of movie fees 
+-- they have received for all their roles combined.  
+-- Only show actors where the total fees are more than $200,000 
+
+
+
+
+-- 3. List the details for the actors that have had roles in movies of 'Comedy' type 
+-- that were made in either 2002 or 2003. Only show each actor's details once.
+ 
+
+
+ 
+-- 4. List the actor number for the actors that have had at least 2 roles in 2002.
+
+
+
+
+-- 5. List the details of the movies that have been directed by Anthony Minghella 
+-- and in which Nicole Kidman has had a role. Sequence by movie title. 
+
+
+
+
+-- 6. Count the number of different actors that have had roles 
+-- in 'Comedy' movies made after 2000.
+
+
+
+
+-- 7. List details for the directors that have ever directed 'Comedy' type movies.
+-- Show each director only once and sequence alphabetically by name.
+
+
+
+
+-- 8. List the movie types that have had between 2 and 4 movies made between 2000 and 2005
+
+
+
+
+--9. Show the number of roles in 2002 for each actor along with the actor number
+-- whose total fees in that year exceeded $100,000
+
+
+
+
+-- 10(i). Write a command to add a new movie to the database  
+
+
+
+
+-- 10(ii). Write a command to double that new movie's number of nominations.
+-- Note 1: Use a numeric expression. Don't calculate manually and hard code the value.
+-- Note 2: We must be able to test and run i) and ii) above in sequence.
+
+
+
